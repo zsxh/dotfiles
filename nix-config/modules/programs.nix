1db71f770002d99a192c01bca96abc010ef4fb30
zsxh <bnbvbchen@gmail.com>
2024-12-31 21:07:10 +0800

    init nix configs

---
+++ nix-config/modules/programs.nix
@@ -0,0 +1,83 @@
+{pkgs, ...}: {
+  # Install packages from nix's official package repository.
+  #
+  # The packages installed here are available to all users, and are reproducible across machines, and are rollbackable.
+  # But on macOS, it's less stable than homebrew.
+  #
+  # Related Discussion: https://discourse.nixos.org/t/darwin-again/29331
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  ];
+  environment.variables.EDITOR = "vim";
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  # this is required if you want to use darwin's default shell - zsh
+  programs.zsh.enable = true;
+  environment.shells = [
+    pkgs.zsh
+  ];
+
+  # NOTE: To make this work, homebrew need to be installed manually, see https://brew.sh
+  #
+  # The apps installed by homebrew are not managed by nix, and not reproducible!
+  # But on macOS, homebrew has a much larger selection of apps than nixpkgs, especially for GUI apps!
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = true; # Fetch the newest stable branch of Homebrew's git repo
+      upgrade = true; # Upgrade outdated casks, formulae, and App Store apps
+      # 'zap': uninstalls all formulae(and related files) not listed in the generated Brewfile
+      # cleanup = "zap";
+    };
+
+    # Applications to install from Mac App Store using mas.
+    # You need to install all these Apps manually first so that your apple account have records for them.
+    # otherwise Apple Store will refuse to install them.
+    # For details, see https://github.com/mas-cli/mas
+    masApps = {
+      # Xcode = 497799835;
+      # Wechat = 836500024;
+      # NeteaseCloudMusic = 944848654;
+      # QQ = 451108668;
+      # WeCom = 1189898970;  # Wechat for Work
+      # TecentMetting = 1484048379;
+      # QQMusic = 595615424;
+    };
+
+    taps = [
+      # "homebrew/services"
+      "d12frosted/emacs-plus"
+    ];
+
+    # `brew install`
+    brews = [
+      # "wget" # download tool
+      # "curl" # no not install curl via nixpkgs, it's not working well on macOS!
+      # "aria2" # download tool
+      # "httpie" # http client
+    ];
+
+    # `brew install --cask`
+    casks = [
+      # "firefox"
+      # "google-chrome"
+      # "visual-studio-code"
+
+      # IM & audio & remote desktop & meeting
+      # "telegram"
+      # "discord"
+
+      # "anki"
+      # "iina" # video player
+      # "raycast" # (HotKey: alt/option + space)search, caculate and run scripts(with many plugins)
+      # "stats" # beautiful system monitor
+      # "eudic" # 欧路词典
+
+      # Development
+      # "insomnia" # REST client
+      # "wireshark" # network analyzer
+    ];
+  };
+}
---
+++ nix-config/modules/system.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}:
+###################################################################################
+#
+#  macOS's System configuration
+#
+#  All the configuration options are documented here:
+#    https://daiderd.com/nix-darwin/manual/index.html
+#  Incomplete list of macOS `defaults` commands :
+#    https://github.com/yannbertrand/macos-defaults
+#
+###################################################################################
+{
+  system = {
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    stateVersion = 5;
+  };
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
